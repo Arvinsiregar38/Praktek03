b564ea754f62ae20c7388cd237ff3fd004ad8b5d
Akhmad Rizwan <34591274+rizwanakhmad10@users.noreply.github.com>
2018-04-06 23:18:21 +0800

    Membuat Atribut dan Behavior

--- src/praktek03/Pesawat.java
+++ src/praktek03/Pesawat.java
@@ -10,5 +10,17 @@ package praktek03;
  * @author Smekma
  */
 public class Pesawat {
+    String Maskapai;
+    String Type;
+    double Harga;
+    String Kelas;
+    String Tujuan;
     
+    void cetakInfo(){
+        System.out.println("Maskapai :"+Maskapai);
+        System.out.println("Type     :"+Type);
+        System.out.println("Harga    :"+Harga);
+        System.out.println("Kelas    :"+Kelas);
+        System.out.println("Tujuan   :"+Tujuan);
+    }
 }
